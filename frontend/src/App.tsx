4439d21cd25f35dbabbbc3b5922a5d90ae614fc5
Yannick Lang <layaxx@gmx.de>
2025-01-21 20:39:42 +0100

    add tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,32 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react"
+import "./App.css"
 
 function App() {
-  const [messages, setMessages] = useState("");
+  const [messages, setMessages] = useState("")
 
   useEffect(() => {
-    const eventSource = new EventSource("http://localhost:3010/events");
+    const eventSource = new EventSource("http://localhost:3010/events")
 
     eventSource.onmessage = (event) => {
-      console.log("Received event:", event.data);
-      setMessages(event.data);
-    };
+      console.log("Received event:", event.data)
+      setMessages(event.data)
+    }
 
     eventSource.onerror = (error) => {
-      console.error("EventSource failed:", error);
-    };
+      console.error("EventSource failed:", error)
+    }
 
     return () => {
-      eventSource.close();
-    };
-  }, []);
+      eventSource.close()
+    }
+  }, [])
 
   return (
     <div>
-      <h1>Real-Time Messages</h1>
+      <h1 className="font-bold">Real-Time Messages</h1>
       <p>{messages}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
