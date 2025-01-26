8a464dc2818c79950183a9e8deeb620283f0bed4
Yannick Lang <layaxx@gmx.de>
2025-01-26 13:17:27 +0100

    chore(frontend): add zod validation

--- frontend/src/VoteApp.tsx
+++ frontend/src/VoteApp.tsx
@@ -1,4 +1,58 @@
+import { useEffect } from "react"
+import { RELAY_SERVER_EVENTS, ResponseSchema } from "./common"
+
+function showMessage(vote: unknown) {
+  console.log("Received vote:", vote)
+}
+
 function VoteApp() {
+  useEffect(() => {
+    console.log("Connecting to EventSource: ", `${RELAY_SERVER_EVENTS}`)
+
+    const eventSource = new EventSource(RELAY_SERVER_EVENTS)
+
+    eventSource.onmessage = (event) => {
+      console.log("Received event:", event.data)
+
+      try {
+        JSON.parse(event.data)
+        const parsedData = JSON.parse(event.data)
+        const { success, data, error } = ResponseSchema.safeParse(parsedData)
+
+        if (!success) {
+          console.error("Failed to parse", data, error)
+          return
+        }
+
+        switch (data.type) {
+          case "complete":
+            console.log("Received complete data, ignoring")
+            break
+          case "partial":
+            console.log("Received partial data")
+            showMessage(data.vote)
+            break
+          case "info":
+            console.log("Received info data")
+            console.info(data.msg)
+            break
+          default:
+            console.error("Unknown response type")
+        }
+      } catch {
+        console.error("Failed to parse JSON")
+      }
+    }
+
+    eventSource.onerror = (error) => {
+      console.error("EventSource failed:", error)
+    }
+
+    return () => {
+      eventSource.close()
+    }
+  }, [])
+
   return (
     <div className="p-4 space-y-4 w-full">
       <h1 className="text-5xl font-bold">Vote</h1>
---
+++ frontend/src/common.ts
@@ -0,0 +1,45 @@
+import { z } from "zod"
+
+export const RELAY_SERVER =
+  import.meta.env.VITE_RELAY_SERVER ?? "http://localhost:3010"
+
+export const RELAY_SERVER_EVENTS = `${RELAY_SERVER}/events`
+
+export enum ResponseType {
+  COMPLETE_DATA = "complete",
+  PARTIAL_DATA = "partial",
+  INFO = "info",
+}
+
+export enum Party {
+  CDU,
+  AFD,
+  SPD,
+  GRUENE,
+  BSW,
+  FDP,
+  LINKE,
+  SONSTIGE,
+}
+
+export const VoteSchema = z.object({
+  id: z.number().min(0),
+  party: z.nativeEnum(Party),
+  cardID: z.string().min(3),
+  createdAt: z.date({ coerce: true }),
+})
+
+export const ResponseSchema = z.discriminatedUnion("type", [
+  z.object({
+    type: z.literal("complete"),
+    votes: z.array(VoteSchema),
+  }),
+  z.object({
+    type: z.literal("partial"),
+    vote: VoteSchema,
+  }),
+  z.object({
+    type: z.literal("info"),
+    msg: z.string(),
+  }),
+])
