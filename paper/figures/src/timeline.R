cfccb30aaf0f2d1a14941094b929b929d2d5a374
Yannick Lang <layaxx@gmx.de>
2025-02-21 11:16:00 +0100

    fix(paper): update timeline visualization

--- paper/figures/src/timeline.R
+++ paper/figures/src/timeline.R
@@ -1,33 +1,24 @@
 library(ggplot2)
 library(dplyr)
-library(forcats)
-library(lubridate)  # For handling timestamps
+library(lubridate)
 
-
-# Read the data
-data <- read.csv("./paper/figures/src/data.csv", header = TRUE, sep = ",")
-
-# Convert timestamps to datetime format & filter out "seeding"
-df_filtered <- data %>%
+# Read and preprocess data
+data <- read.csv("./paper/figures/src/data.csv", sep = ",") %>%
   filter(cardID != "seeding") %>%
   mutate(createdAt = as.POSIXct(createdAt / 1000, origin = "1970-01-01"))
 
-# Define the color mapping
-party_colors <- c(
-  "CDU" = "#000000",
-  "AFD" = "#0000ff",
-  "SPD" = "#ff0000",
-  "GRUENE" = "#008000",
-  "BSW" = "#7b2450",
-  "FDP" = "#ffff00",
-  "LINKE" = "#ff00ff",
-  "FW" = "#f59a00",
-  "SONSTIGE" = "#c0c0c0"
-)
+# Define colors
+party_colors <- c("CDU" = "#000000", "AFD" = "#0000ff", "SPD" = "#ff0000", 
+                  "GRUENE" = "#008000", "BSW" = "#7b2450", "FDP" = "#ffff00", 
+                  "LINKE" = "#ff00ff", "FW" = "#f59a00", "SONSTIGE" = "#c0c0c0")
 
-# Create the timeline plot
-ggplot(df_filtered, aes(x = createdAt, y = party, color = party)) +
-  geom_point(size = 3) +  # Plot points
-  scale_color_manual(values = party_colors) +  # Apply custom colors
-  labs(title = "Timeline", x = "Time", y = "Partei") + 
-  theme(legend.position = "none")
+# Plot timeline with all points on the same y-axis level
+ggplot(data, aes(x = createdAt, y = 0, color = party)) +
+  geom_jitter(height = .1, size = 3) +  # Add jitter to prevent overlap
+  scale_color_manual(values = party_colors) +
+  labs(title = "Timeline registrierter Abstimmungen", x = "Zeit", y = NULL) +
+  theme_minimal() +
+  theme(
+      axis.text.y = element_blank(),  # Remove y-axis text
+
+  )
